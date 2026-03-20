e506feab9e24f7fb4039d6baa5d7b46715c68b4d
aditi-2611 <aditi.mish2004@gmail.com>
2026-03-20 18:10:05 +0530

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,7 +1,7 @@
 const { Pool } = require("pg");
 
 const pool = new Pool({
-  connectionString: process.env.postgresql://root:uJNauMKOtRIPKXBpaaKnFehDfCJkJBaz@dpg-d6uivrq4d50c73f5kb8g-a.oregon-postgres.render.com/fresh_market_fxfg,
+  connectionString: process.env.DB_URL,
   ssl: {
     rejectUnauthorized: false,
   },
