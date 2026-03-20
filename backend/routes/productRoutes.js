0af5e531f3688c09723a59cee000dec1a826887f
aditi-2611 <aditi.mish2004@gmail.com>
2026-03-20 17:41:52 +0530

    Update productRoutes.js

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,8 +1,14 @@
 const express = require("express");
 const router = express.Router();
-const { getProducts, getSingleProduct } = require("../controllers/productController");
+
+const {
+  getProducts,
+  getSingleProduct,
+  getProductsByCategory,
+} = require("../controllers/productController");
 
 router.get("/", getProducts);
+router.get("/category/:category", getProductsByCategory);
 router.get("/:id", getSingleProduct);
 
 module.exports = router;
